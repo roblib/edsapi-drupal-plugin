f041875e4e61303ff280c0c6c676e7ec1d89e237
Rui Francisco <blackmolly@gmail.com>
2015-03-04 18:04:48 +0000

    Added License

--- ebsco/templates/ebsco-basic-search.tpl.php
+++ ebsco/templates/ebsco-basic-search.tpl.php
@@ -5,6 +5,20 @@
  * Displays the basic search form block.
  *
  * @see template_preprocess_ebsco_basic_search()
+ *
+ * Copyright [2014] [EBSCO Information Systems]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
  */
 ?>
 
